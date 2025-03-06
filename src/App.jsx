f650261485c83e0f2dd4932c39183d6b19eb7079
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2025-03-06 19:58:06 +0300

    add button modules

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import Food from "./Food.jsx"
 import Card from "./Card.jsx"
+import Button from "./Button/Button.jsx"
 
 function App() {
   return (
     <>
     <Header />
-    <Food/>
+  
+    <Button/>
     <Card/>
     <Card/>
     <Card/>
-    
+
     <Footer />
     </>
   )
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,12 @@
-
+import "./Header.css"
 
 const Header = () => {
     return (
         <header>
             <h1>My First React App</h1>
         
-        <nav>
-            <ul>
+        <nav className="nav-bar">
+            <ul className="nav-links">
                 <li ><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>  
                 <li><a href="#">Services</a></li>
