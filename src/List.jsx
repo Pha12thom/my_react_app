8c3f4eab46bde0f9759b2ce963eee9e6b7467c4c
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2025-03-07 03:18:35 +0300

    comiit list

--- src/List.jsx
+++ src/List.jsx
@@ -1,11 +1,27 @@
 const List = () => {
     const Fruits= [
-        {name: "banana", calories: 30}, 
-        {name: "apple", calories: 40}, 
-        {name: "orange", calories: 20},
+        {id: 1, name: "banana", calories: 30}, 
+        {id: 2, name: "apple", calories: 40}, 
+        {id: 3, name: "orange", calories: 20},
     ];
-    const Items = Fruits.map(fruit => <li key={fruit.name}>{fruit.name} - {fruit.calories}</li>)
-    
+
+    Fruits.sort((a,b) => a.name.localeCompare(b.name));//sort with name
+    Fruits.sort((a,b) => b.name.localeCompare(a.name));//sort from large letter
+    Fruits.sort((a,b) => a.calories - b.calories);
+
+
+
+
+    Fruits.sort((a,b) => b.calories - a.calories);
+
+    const lowercales = Fruits.filter(fruit => fruit.calories < 30);
+    const Highcal = Fruits.filter(fruit => fruit.calories > 30);
+
+    //const Items = Fruits.map(fruit => <li key={fruit.id}>{fruit.name} - {fruit.calories}</li>)
+
+    //const Items = lowercales.map(lowercale => <li key={lowercale.id}>{lowercale.name} - {lowercale.calories}</li>)
+    const Items = Highcal.map(Highcal => <li key={Highcal.id}>{Highcal.name} - {Highcal.calories}</li>)
+
     return (<ol>{Items}</ol>);
 
 
